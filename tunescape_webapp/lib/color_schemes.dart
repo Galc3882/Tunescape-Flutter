14364415ebddfa33ab99d4ad7595ba5f56a612a0
Galc3882 <86870298+Galc3882@users.noreply.github.com>
2022-06-22 17:19:24 -0400

    Implemented theme and first iteration of front page

---
+++ tunescape_webapp/lib/color_schemes.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: lightColorScheme,
+  fontFamily: 'Lato',
+  textTheme: lightTextScheme,
+);
+final darkTheme = ThemeData(
+  colorScheme: darkColorScheme,
+  fontFamily: 'Lato',
+  textTheme: darkTextScheme,
+);
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF2D5DAA),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFD7E2FF),
+  onPrimaryContainer: Color(0xFF001A40),
+  secondary: Color(0xFF315DA8),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFD8E2FF),
+  onSecondaryContainer: Color(0xFF001A41),
+  tertiary: Color(0xFF715574),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFFBD7FC),
+  onTertiaryContainer: Color(0xFF29132D),
+  error: Color(0xFFBA1A1A),
+  errorContainer: Color(0xFFFFDAD6),
+  onError: Color(0xFFFFFFFF),
+  onErrorContainer: Color(0xFF410002),
+  background: Color(0xFFFEFBFF),
+  onBackground: Color(0xFF1B1B1F),
+  surface: Color(0xFFFEFBFF),
+  onSurface: Color(0xFF1B1B1F),
+  surfaceVariant: Color(0xFFE1E2EC),
+  onSurfaceVariant: Color(0xFF44474F),
+  outline: Color(0xFF74777F),
+  onInverseSurface: Color(0xFFF2F0F4),
+  inverseSurface: Color(0xFF2F3033),
+  inversePrimary: Color(0xFFACC7FF),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF2D5DAA),
+);
+final lightTextScheme = TextTheme(
+  titleLarge: TextStyle(fontSize: 108.0, color: lightColorScheme.onPrimary),
+  displayMedium: TextStyle(
+      fontSize: 502.0,
+      fontWeight: FontWeight.bold,
+      color: lightColorScheme.onPrimary),
+  bodyMedium: TextStyle(fontSize: 104.0, color: lightColorScheme.onPrimary),
+  labelSmall: TextStyle(fontSize: 16.0, color: lightColorScheme.inverseSurface),
+);
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFFACC7FF),
+  onPrimary: Color(0xFF002F68),
+  primaryContainer: Color(0xFF054490),
+  onPrimaryContainer: Color(0xFFD7E2FF),
+  secondary: Color(0xFFADC6FF),
+  onSecondary: Color(0xFF002E69),
+  secondaryContainer: Color(0xFF0F448E),
+  onSecondaryContainer: Color(0xFFD8E2FF),
+  tertiary: Color(0xFFDEBCDF),
+  onTertiary: Color(0xFF402844),
+  tertiaryContainer: Color(0xFF583E5B),
+  onTertiaryContainer: Color(0xFFFBD7FC),
+  error: Color(0xFFFFB4AB),
+  errorContainer: Color(0xFF93000A),
+  onError: Color(0xFF690005),
+  onErrorContainer: Color(0xFFFFDAD6),
+  background: Color(0xFF1B1B1F),
+  onBackground: Color(0xFFE3E2E6),
+  surface: Color(0xFF1B1B1F),
+  onSurface: Color(0xFFE3E2E6),
+  surfaceVariant: Color(0xFF44474F),
+  onSurfaceVariant: Color(0xFFC4C6D0),
+  outline: Color(0xFF8E9099),
+  onInverseSurface: Color(0xFF1B1B1F),
+  inverseSurface: Color(0xFFE3E2E6),
+  inversePrimary: Color(0xFF2D5DAA),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFFACC7FF),
+);
+final darkTextScheme = TextTheme(
+  titleLarge: TextStyle(fontSize: 108.0, color: darkColorScheme.onPrimary),
+  displayMedium: TextStyle(
+      fontSize: 502.0,
+      fontWeight: FontWeight.bold,
+      color: lightColorScheme.onPrimary),
+  bodyMedium: TextStyle(fontSize: 104.0, color: darkColorScheme.onPrimary),
+  labelSmall: TextStyle(fontSize: 20.0, color: darkColorScheme.inverseSurface),
+);
--- tunescape_webapp/lib/main.dart
+++ tunescape_webapp/lib/main.dart
@@ -1,121 +1,119 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'color_schemes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
+final _key = GlobalKey();
 void main() {
-  runApp(const MyApp());
+  runApp(const MediaQuery(data: MediaQueryData(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     // hide keyboard in case of overflow
     // final isKeyboard = MediaQuery.of(context).viewInsets.bottom != false;
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.orange,
-        scaffoldBackgroundColor: const Color.fromRGBO(45, 93, 170, 1),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: MediaQuery.of(context).platformBrightness == Brightness.dark
+          ? darkTheme
+          : lightTheme,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(fontFamily: 'Lato', fontSize: 55),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Padding(
+                padding: const EdgeInsets.all(35.0),
+                child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      'Tunescape',
+                      style: Theme.of(context).textTheme.titleLarge,
+                      maxLines: 1,
+                    ))),
+            SizedBox(
+                height: MediaQuery.of(context).size.height * 1 / 3,
+                child: Center(
+                    child: buildFloatingSearchBar(
+                  context,
+                  // _key,
+                ))),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget buildFloatingSearchBar(context) {
+  // final size = key.currentContext!.size;
+  return FloatingSearchBar(
+    hint: // size == null
+        // ? 'Search'
+        // : size.width > 50
+        'Search for a song... or piece... or artist...',
+    // : 'Search for a song...',
+    hintStyle: Theme.of(context).textTheme.labelSmall,
+    scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+    transitionDuration: const Duration(milliseconds: 800),
+    transitionCurve: Curves.easeInOut,
+    physics: const BouncingScrollPhysics(),
+    axisAlignment: 0.0,
+    openAxisAlignment: 0.0,
+    width: 600,
+    debounceDelay: const Duration(milliseconds: 500),
+    onQueryChanged: (query) {
+      // Call your model, bloc, controller here.
+    },
+    // Specify a custom transition to be used for
+    // animating between opened and closed stated.
+    transition: CircularFloatingSearchBarTransition(),
+    actions: [
+      FloatingSearchBarAction(
+        showIfOpened: false,
+        child: CircularButton(
+          icon: const Icon(Icons.gpp_maybe_sharp),
+          onPressed: () {},
+        ),
+      ),
+      FloatingSearchBarAction.searchToClear(
+        showIfClosed: false,
+      ),
+    ],
+    builder: (context, transition) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Material(
+          color: Colors.white,
+          elevation: 4.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: Colors.accents.map((color) {
+              return Container(height: 112, color: color);
+            }).toList(),
+          ),
+        ),
+      );
+    },
+  );
 }
